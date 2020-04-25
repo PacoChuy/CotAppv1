5ac8120ff51d554a0192aeb6fb33b208dfb1f799
PacoChuy <p315226@uach.mx>
2020-04-24 21:32:56 -0600

    llenado del spinner

--- app/src/main/java/com/example/cotapp/Registro_Usuario.java
+++ app/src/main/java/com/example/cotapp/Registro_Usuario.java
@@ -27,6 +27,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -35,9 +36,11 @@ import java.util.Map;
 
 public class Registro_Usuario extends AppCompatActivity {
 
-
+    List<String>listaIdSucursales; //guarda el id de los Roles
+    List<String>listaRoles;// guarda el nombre de la sucursal
     EditText nombre,apellido,correo,password,confirm_password;
     Button btnAgregar;
+    Spinner spinnerRol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +53,8 @@ public class Registro_Usuario extends AppCompatActivity {
         password=(EditText)findViewById(R.id.txt_password);
         confirm_password=(EditText)findViewById(R.id.txt_con_pass);
         btnAgregar=(Button) findViewById(R.id.btn_registar);
-
-
+        spinnerRol=(Spinner)findViewById(R.id.sp_Rol);
+        cargarRoles();
 
         btnAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,6 +72,57 @@ public class Registro_Usuario extends AppCompatActivity {
     }
 
 
+    public void cargarRoles(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+    String url ="http://192.168.0.5/coti/controller/Fill_Spinner_Rol.php?funcion=obtenerRol";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            if (response.length()>0){
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    obtenerRoles(jsonArray);
+
+                }catch (JSONException jsnex1){
+
+                    Toast.makeText(getApplicationContext(),jsnex1.toString(),Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+
+        }
+    }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_LONG).show();
+
+        }
+    });
+    queue.add(stringRequest);
+
+    }
+
+    public void  obtenerRoles(JSONArray jsonArray){
+        listaIdSucursales=new ArrayList<String>();
+        listaRoles = new ArrayList<String>();
+        for(int i=0; i<jsonArray.length(); i++){
+            try{
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String id = jsonObject.getString("id");
+                String Rol =jsonObject.getString("Rol");
+                listaIdSucursales.add(id);
+                listaRoles.add(Rol);
+            }catch (JSONException jsnEx2){
+                Toast.makeText(getApplicationContext(),jsnEx2.toString(),Toast.LENGTH_LONG).show();
+
+            }
+
+        }
+        ArrayAdapter<String>adapterRoles = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,listaRoles);
+        spinnerRol.setAdapter(adapterRoles);
+
+    }
 
     private void  ejecutarServicio(String URL)
     {
