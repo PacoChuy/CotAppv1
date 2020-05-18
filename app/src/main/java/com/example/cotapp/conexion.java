3f2e83388b9dfb20a8d87c7c4c8b2d3d0fb262a1
PacoChuy <p315226@uach.mx>
2020-05-18 00:17:07 -0600

    se integra la conexion al servidor en la nuve

--- app/src/main/java/com/example/cotapp/conexion.java
+++ app/src/main/java/com/example/cotapp/conexion.java
@@ -1,5 +1,5 @@
 package com.example.cotapp;
 
 public class conexion {
-    public static final String URL_WEB_SERVICES ="http://192.168.0.4/coti/controller/";
+    public static final String URL_WEB_SERVICES ="http://skinbeauty-spa.com/coti/controller/";
 }
